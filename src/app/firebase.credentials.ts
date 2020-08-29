95e5cc0f2bc73a0e81f2a8e6152ca125564cffed
Aditya V Nadkarni <55201143+Avn007@users.noreply.github.com>
2020-08-29 21:53:37 +0530

    Update firebase.credentials.ts

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -9,14 +9,6 @@ export const FIREBASE_CONFIG = {
 // <script>
   // Your web app's Firebase configuration
 
-    apiKey: "AIzaSyB8PlqPxEhiQe-olWwXfPFjj5cJZ_fHAZc",
-    authDomain: "qnaforum-eb80a.firebaseapp.com",
-    databaseURL: "https://qnaforum-eb80a.firebaseio.com",
-    projectId: "qnaforum-eb80a",
-    storageBucket: "qnaforum-eb80a.appspot.com",
-    messagingSenderId: "91439900223",
-    appId: "1:91439900223:web:aeb92937e2634783c87a8a",
-    measurementId: "G-09DSW7XS7D"
     
   // Initialize Firebase
 //   firebase.initializeApp(firebaseConfig);
